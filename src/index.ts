1b6292ca03a75f4d2e38c0cb29b660779fa320c4
Gary Miller <gary@helixta.com.au>
2022-08-30 15:49:06 +1000

    added index.ts made tsconfig composite: true

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export * from './lib/veditor/type';
+export {
+    createVEditor,
+    Factory,
+    FieldEditorProps,
+    StructEditorProps,
+    UnimplementedEditorProps,
+    UnionEditorProps,
+} from './lib/veditor/adlfactory';
+export { useSelectState, SelectState } from "./lib/select";
